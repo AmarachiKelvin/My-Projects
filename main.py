87596e828ed746ccc2204d0dcb8755c58e9ac700
AmarachiKelvin <90745102+AmarachiKelvin@users.noreply.github.com>
2022-09-12 00:50:40 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+# End of the year appreciation
+gratitude = 'Thank you all'
+print("""It has been a wonderful year
+y'all have been really amazing in one way or the other, 
+and I'm so happy our parts crossed,
+I wish you all a better year ahead!""")
+
+first_name = 'Amarachi'
+last_name = 'Kelvin'
+
+print(f"{first_name } {last_name } love you all")
+print(gratitude)
+
+
